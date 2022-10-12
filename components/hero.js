76b2d9ec68252bafdea3d3173760dde2bf67b27e
shuman7 <shuman1231@gmail.com>
2022-10-12 22:54:16 +0900

    first commit Done Chapter3

---
+++ components/hero.js
@@ -0,0 +1,13 @@
+import styles from 'styles/hero.module.css'
+
+export default function Hero({ title, subtitle, imageOn = false }) {
+    return (
+        <div>
+          <div className={styles.text}>
+            <h1 className={styles.title}>{title}</h1>
+            <p className={styles.subtitle}>{subtitle}</p>
+          </div>
+            {imageOn && <figure> [画像] </figure>}
+        </div>
+    )
+}
